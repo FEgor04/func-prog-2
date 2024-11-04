284588eb727759755cbb39332f5633e108440b00
Egor Fedorov <fegor2004@gmail.com>
2024-11-04 12:33:12 +0300

    remove binary tree

--- lib/btree.ml
+++ lib/btree.ml
@@ -4,102 +4,32 @@ module type OrderedType = sig
   val compare : t -> t -> int
 end
 
+module type BTreeConfig = sig
+  val t : int
+end
+
 module type Dict = sig
   type key
   type 'a t
 
   val empty : 'a t
+  val singleton : key -> 'a -> 'a t
+
+
   val is_empty : 'a t -> bool
-  val add : 'a t -> key -> 'a -> 'a t
-  val length : 'a t -> int
-  val of_list : (key * 'a) list -> 'a t
-  val to_list : 'a t -> (key * 'a) list
-  val map : (key -> 'a -> 'b) -> 'a t -> 'b t
-  val fold_left : ('acc -> key * 'a -> 'acc) -> 'acc -> 'a t -> 'acc
-  val fold_right : ('acc -> key * 'a -> 'acc) -> 'acc -> 'a t -> 'acc
-  val union : 'a t -> 'a t -> 'a t
-  val filter : (key * 'a -> bool) -> 'a t -> 'a t
-  val has : key -> 'a t -> bool
-  val get : key -> 'a t -> 'a option
+  val find : key -> 'a t -> 'a option
 end
 
-module Make (Ord : OrderedType) : Dict with type key = Ord.t = struct
+module Make (Ord : OrderedType) (Config : BTreeConfig) :
+  Dict with type key = Ord.t = struct
   type key = Ord.t
-  type 'a t = Empty | Node of { l : 'a t; k : key; v : 'a; r : 'a t }
+  type 'a t = Empty | Node of { children : 'a t list; keys : (key * 'a) list }
 
   let empty = Empty
   let is_empty = function Empty -> true | _ -> false
+  let singleton k v = Node { children = []; keys = [ (k, v) ] }
 
-  let rec add t key value =
-    match t with
-    | Empty -> Node { l = Empty; k = key; v = value; r = Empty }
-    | Node t when Ord.compare t.k key < 0 ->
-        Node { t with l = add t.l key value }
-    | Node t when Ord.compare t.k key > 0 ->
-        Node { t with r = add t.r key value }
-    | Node t when Ord.compare t.k key = 0 -> Node { t with v = value }
-    | _ -> Empty
-
-  let rec add_preserve t key value =
-    match t with
-    | Empty -> Node { l = Empty; k = key; v = value; r = Empty }
-    | Node t when Ord.compare t.k key < 0 ->
-        Node { t with l = add_preserve t.l key value }
-    | Node t when Ord.compare t.k key > 0 ->
-        Node { t with r = add_preserve t.r key value }
-    | Node t when Ord.compare t.k key == 0 -> Node t
-    | _ -> Empty
-
-  let rec length = function Node n -> 1 + length n.l + length n.r | Empty -> 0
-
-  let rec tail_of_list lst acc =
-    match lst with
-    | [] -> acc
-    | (key, value) :: tl -> tail_of_list tl (add acc key value)
-
-  let of_list lst = tail_of_list lst Empty
-
-  let rec acc_to_list t acc =
-    match t with
-    | Empty -> acc
-    | Node n -> acc_to_list n.l [] @ acc @ [ (n.k, n.v) ] @ acc_to_list n.r []
-
-  let to_list t = acc_to_list t [] |> List.rev
-
-  let rec map f t =
-    match t with
-    | Node n ->
-        let l' = map f n.l in
-        let r' = map f n.r in
-        let v' = f n.k n.v in
-        Node { n with l = l'; r = r'; v = v' }
-    | Empty -> Empty
-
-  let rec fold_left f acc t =
-    match t with
-    | Node { l; k; v; r } -> fold_left f (f (fold_left f acc l) (k, v)) r
-    | Empty -> acc
-
-  let rec fold_right f acc t =
-    match t with
-    | Node { l; k; v; r } -> fold_right f (f (fold_right f acc r) (k, v)) l
-    | Empty -> acc
-
-  let rec union d1 = function
-    | Node { l; r; k; v } -> union (union (add_preserve d1 k v) l) r
-    | Empty -> d1
-
-  let rec filter f = function
-    | Node n when f (n.k, n.v) ->
-        Node { n with l = filter f n.l; r = filter f n.r }
-    | Node n -> union (filter f n.l) (filter f n.r)
-    | _ -> Empty
-
-  let rec get key = function
-    | Node { l = _; r = _; k; v } when Ord.compare key k = 0 -> Some v
-    | Node { l; r = _; k; v = _ } when Ord.compare key k < 0 -> get key l
-    | Node { l = _; r; k; v = _ } when Ord.compare key k > 0 -> get key r
-    | _ -> None
-
-  let has k t = Option.is_some (get k t)
-end
+  let find _ = function
+    | Empty -> None
+    | Node{children=_; keys=_} -> None
+  end
--- lib/btree.mli
+++ lib/btree.mli
@@ -4,39 +4,25 @@ module type OrderedType = sig
   val compare : t -> t -> int
 end
 
-(** Proof that [Dict] is a monoid.
-  Consider M to be a set of all possible finite dictionaries with same key and value type.`
-  Consider a [union] to be a binary operation on M.
-  It is obviously associative: (x ∪ y) ∪ z = x ∪ (y ∪ z)
-  A neutral element is [Dict.Empty]: {} ∪ x = x ∪ {} = x
-    *)
+module type BTreeConfig = sig
+  val t : int
+end
+
 module type Dict = sig
   type key
   type 'a t
 
   val empty : 'a t
-  (** Empty dictionary **)
+  (** Empty dictionary *)
+
+  val singleton : key -> 'a -> 'a t
+  (** Dictionary with only one key *)
 
   val is_empty : 'a t -> bool
-  (** Checks if given dictionary is empty **)
+  (** Checks if given dictionary is empty *)
 
-  val add : 'a t -> key -> 'a -> 'a t
-  (** Adds new key=value pair to given dictionary **)
-
-  val length : 'a t -> int
-  (** Returns number of elemnst in given dictionary *)
-
-  val of_list : (key * 'a) list -> 'a t
-  (** Creates a new tree from given list **)
-
-  val to_list : 'a t -> (key * 'a) list
-  val map : (key -> 'a -> 'b) -> 'a t -> 'b t
-  val fold_left : ('acc -> key * 'a -> 'acc) -> 'acc -> 'a t -> 'acc
-  val fold_right : ('acc -> key * 'a -> 'acc) -> 'acc -> 'a t -> 'acc
-  val union : 'a t -> 'a t -> 'a t
-  val filter : (key * 'a -> bool) -> 'a t -> 'a t
-  val has : key -> 'a t -> bool
-  val get : key -> 'a t -> 'a option
+  val find : key -> 'a t -> 'a option
+  (** Returns value associated with given key *)
 end
 
-module Make (Ord : OrderedType) : Dict with type key = Ord.t
+module Make (Ord : OrderedType) (_ : BTreeConfig) : Dict with type key = Ord.t
